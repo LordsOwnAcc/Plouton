4da4f654d1757ba2309018ed21a72c8b9c5d96a4
LordsOwnAcc <sumity12019@gmail.com>
2025-08-02 11:06:07 +0530

    Added Repo

---
+++ app/src/main/java/com/plouton/CiviLized/ui/theme/Presentation/HomePAge.kt
@@ -0,0 +1,26 @@
+package com.plouton.CiviLized.ui.theme.Presentation
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.modifier.modifierLocalConsumer
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun HomePage(modifier: Modifier = Modifier) {
+    Column(Modifier.fillMaxSize().background(Color.White)){
+
+
+
+    }
+}
+
+
+@Preview
+@Composable
+fun HomePagePre(modifier: Modifier = Modifier) {
+    HomePage()
+}
